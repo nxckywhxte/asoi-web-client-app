906c1f9f1373c0be5e262568757a3dcdfc20910d
Nikita Belonogov <crcku.mark00@gmail.com>
2023-04-27 13:07:36 +0300

    project init

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,13 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  useTabs: true,
+  arrowParens: "avoid",
+  jsxSingleQuote: true,
+  semi:false,
+  singleQuote:true,
+  trailingComma:"none",
+  singleAttributePerLine:true,
 };
 
 module.exports = config;
